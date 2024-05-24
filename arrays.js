4220596d318b18704fc00017526f237ae202e56f
maurash01 <ashumaurya023@gmail.com>
2024-05-24 19:06:24 +0530

    Arrays In JS

--- arrays.js
+++ arrays.js
@@ -21,3 +21,18 @@ console.log(movies.slice(1));
 const price = '₹536.4565/-';
 console.log(price.slice(1,7));
 
+//adding new elements 
+movies.push('The Dark Knight');// new element at the end of the array 
+movies.unshift('The Godfather'); // new element at the starting of an array
+console.log(movies);
+
+// removieng elements
+movies.pop();
+movies.shift()
+console.log(movies);
+//  movies.splice(3,2);       // It will remove 2 elements starting from index 3 
+
+//movies.splice(3,2, 'Batman', 'Superman', 'Flash'); // It will remove 2 elements starting from index 3 and add 3 new elements.
+//console.log(movies);
+movies.splice(3,2, 'Batman', 'Superman', 'Flash');
+console.log(movies); // adding new elements without removing elements.
